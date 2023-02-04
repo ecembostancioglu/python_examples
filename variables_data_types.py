1b8dc1bbead74cbf1788f53cabc809f9d588281d
ecembostancioglu <ecembostancioglu21@gmail.com>
2023-02-04 19:13:03 +0300

    Added variables

---
+++ variables_data_types.py
@@ -0,0 +1,16 @@
+a=3   #int
+b=3.14   #float
+c="Python"   #Str
+d=[1,2,3,4,5,"Python"]   #List
+e=(1,2,3,4,5,"Python")   #Tuple
+f={"Apple":3,"Banana":4,"Strawberry":5}   #Dictionary
+g=True   #bool
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+
