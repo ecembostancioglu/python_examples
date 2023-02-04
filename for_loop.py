63357e1fcf9665f98e045b3e33a065175e00e613
ecembostancioglu <ecembostancioglu21@gmail.com>
2023-02-04 20:52:18 +0300

    Added for loop

---
+++ for_loop.py
@@ -0,0 +1,12 @@
+a=[1,2,3,4,5,6]
+b="Python"
+
+for ch in b:
+    print(ch)   #P y t h o n
+
+for number in range(20,30):
+    print(number)   #20 21 22 23 24 25 26 27 28 29
+
+
+for number1 in range(0,40,4):
+    print(number1)   #0 4 8 12 16 20 24 28 32 36
