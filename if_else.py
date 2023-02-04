d6cc529d923d4cf62166da09cd8de5551553e17d
ecembostancioglu <ecembostancioglu21@gmail.com>
2023-02-04 19:53:56 +0300

    Added if else

---
+++ if_else.py
@@ -0,0 +1,18 @@
+age=int(input("Enter your age:"))
+
+if age<18:
+    print("You are young")
+else:
+    print("You are adult")
+
+
+operation=int(input("Enter the operation:"))
+
+if operation == 1:
+    print("You choose 1")
+elif operation == 2:
+    print("You choose 2")
+elif operation == 3:
+    print("You choose 3")
+else:
+    print("Invalid")
