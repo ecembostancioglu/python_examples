d4fd2efcf2f2dbd7e263f313df194c1cf30a0e39
ecembostancioglu <ecembostancioglu21@gmail.com>
2023-02-04 19:36:58 +0300

    Added indexing Strings and Lists

---
+++ indexing_str_list.py
@@ -0,0 +1,21 @@
+a="Python"
+b= [1,2,3,4,5,6,7] 
+
+print(a[0])   #P
+print(a[2])   #t
+print(len(a))   #6
+print(len(b))   #7
+print(a[len(a)-1])   #n
+print(b[len(b)-1])   #7
+print(a[0:2])   #Py
+print(a[2:])   #thon
+print(a[:4])   #Pyth
+print(b[2:])   #[3, 4, 5, 6, 7]
+print(b[0:6:2])   #[1, 3, 5]
+print(b[::2])   #[1, 3, 5, 7]
+
+
+c={"Apple":3,"Banana":4,"Strawberry":5}
+
+print(c["Apple"])   #3
+print(c["Orange"])   #KeyError
