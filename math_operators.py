d32b4b75c5f15f3c7138ad0261ad9970b8d9265a
ecembostancioglu <ecembostancioglu21@gmail.com>
2023-02-04 19:22:48 +0300

    Added math operators

---
+++ math_operators.py
@@ -0,0 +1,19 @@
+a = 5
+b = 10
+c= a + 2 * b 
+
+print(c)
+
+d="Python "
+e="Programming "
+f="Language"
+
+g=d+e+f
+
+print(g)
+
+h="Python"
+
+print(h * 5)   #PythonPythonPythonPythonPython
+
+print("*" * 4)   #****
