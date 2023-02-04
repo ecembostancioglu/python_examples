914b7a1790a652a9b3964bde3c035468a7bacace
ecembostancioglu <ecembostancioglu21@gmail.com>
2023-02-04 19:46:44 +0300

    Added inputs

---
+++ inputs.py
@@ -0,0 +1,15 @@
+age=input("Enter your age:")
+print("Your age is",age)
+
+a=input("a:")
+b=input("b:")
+c=input("c:")
+print("Total is",a+b+c)   #a=1, b=2, c=3 -> Total is 123
+
+d=int(input("d:"))
+e=int(input("e:"))
+f=int(input("f:"))
+
+
+print("Total is", d+e+f)   #Total is 6
+
