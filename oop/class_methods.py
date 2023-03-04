50a65413a9e6652f32f21f930fddf10074eba534
ecembostancioglu <ecembostancioglu21@gmail.com>
2023-03-04 23:05:36 +0300

    Added subclass

--- oop/class_methods.py
+++ oop/class_methods.py
@@ -60,4 +60,57 @@ new_date=datetime.date(2023,6,18)
 print(new_date.day)
 print(Staff.working_day(new_date))
 
+print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+
+class Developer(Staff): #Inheritance
+   raiseRate=1.1
+
+   def __init__(self,name,surname,salary,language):
+      super().__init__(name,surname,salary)
+      self.language=language
+      print(f'New staff was moved to Developer class: {self.name} {self.surname}')
+      
+
+
+dev_1=Developer('Dean','Winchester',10000,'python')
+dev_2=Developer('Sam','Winchester',15000,'java')
+
+print(dev_1.email)
+print(dev_1.salary)
+print(dev_1.language)
+
+   
+class Manager(Staff):
+   
+   def __init__(self, name, surname, salary,workers=None):
+      super().__init__(name, surname, salary)
+      if workers is None:
+         self.workers=[]
+      else:
+         self.workers=workers
+        
+
+   def addWorker(self,worke):
+    if worke not in self.workers: 
+      self.workers.append(worke)
+
+   def deleteWorker(self,worke):
+    if worke in self.workers: 
+      self.workers.remove(worke)
+
+   
+   def listWorkers(self):
+     for e, worke in enumerate(self.workers):
+        e += 1
+        print(e, worke.fullName()) 
+ 
+
+manager_1=Manager('John','Wick',50000,[dev_1])
+manager_2=Manager('John','Snow',50000)
+
+print('_______________________')
+manager_1.listWorkers()
+print('_______________________')
+manager_1.addWorker(dev_2)
+manager_1.listWorkers()
 
